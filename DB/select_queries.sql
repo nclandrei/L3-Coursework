e1b621dbf252a33d36e81df5772b8edc1a465143
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2016-02-16 03:27:37 +0000

    Finished the select sql.

--- DB/select_queries.sql
+++ DB/select_queries.sql
@@ -1,12 +1,12 @@
-﻿-- Query 1: selects the name from members table and checks if it is alive and the name starts with Tim
+﻿-- Query 1: SELECTs the name FROM members table and checks if it is alive and the name starts with Tim
 
 SELECT name
 FROM members
 WHERE stillalive='Y' AND name LIKE 'Tim%';
 
--- Query 2: firstly, it selects the band id from bands table that corresponds to Iron Maiden;
---          afterwards, it selects all the member ids from memberof table that belong to the band;
---          lastly, it takes their names from the members table which have their ids as the ones retrieved from the step above
+-- Query 2: firstly, it SELECTs the band id FROM bands table that corresponds to Iron Maiden;
+--          afterwards, it SELECTs all the member ids FROM memberof table that belong to the band;
+--          lastly, it takes their names FROM the members table which have their ids as the ones retrieved FROM the step above
 
 
 SELECT name
@@ -18,61 +18,64 @@ WHERE mid IN (SELECT mid
 			  WHERE name='Iron Maiden') AND endyear IS NULL);
 
 
--- Query 3: selects all members' names and checks which band names are in the results retrieved
+-- Query 3: SELECTs all members' names and checks which band names are in the results retrieved
 
 SELECT name
 FROM bands
-WHERE name IN (select name
-	    from members
+WHERE name IN (SELECT name
+	    FROM members
 	    );
 
 
--- Query 4: takes from the memberof table all the tuples that have instrument equal to bass and counts the tuples retrieved
+-- Query 4: takes FROM the memberof table all the tuples that have instrument equal to bass and counts the tuples retrieved
 
-SELECT COUNT(*) AS Bass_Player_Count
+SELECT COUNT(DISTINCT mid) AS Bass_Player_Count
 FROM memberof
 WHERE instrument='bass';
 
 
--- Query 5: ???
+-- Query 5: gets all drummers from the cartesian product of memberof, releases and members, and then checks if
+--          the release was launched while the drummer was part of the band; lastly, we order at first by the 
+--          drummer's name (such that all drummer's releases are grouped together), and then by year; a group by
+--          clause on the drummer name could not be added here as we have no aggregate functions to apply on the other
+--          2 columns in the select clause (Release_Name and Year)
 
-SELECT DISTINCT m.name AS Drummer_Name, r.title AS Release_Name, r.year AS Year
-FROM members m
-JOIN memberof mo ON m.mid = mo.mid
-JOIN releases r ON r.bid = mo.bid
-WHERE mo.instrument='drums'
-ORDER BY m.name,year;
+SELECT members.name AS Drummer_Name, releases.title AS Release_Name, releases.year AS Year
+FROM members,releases,memberof
+WHERE releases.bid= memberof.bid AND members.mid = memberof.mid AND memberof.instrument='drums' AND
+((memberof.startyear IS NULL AND memberof.endyear IS NULL) OR
+(memberof.startyear <= releases.year AND  memberof.endyear is NULL) OR
+(memberof.startyear IS NULL AND  releases.year <= memberof.endyear) OR
+(memberof.startyear <= releases.year AND  releases.year <= memberof.endyear))
+ORDER BY Drummer_Name, Year;
 
 
 -- Query 6: firstly, it joins releases on band id, afterwards joining songs on the release id;
 --          then, we check which tuples have cdbonus equal to yes and the rating is either null or greater or equal than 5,
 --          finally grouping them by band name and ordering it by band name as well;
 
-select b.name as Band_Name, count(cdbonus) as Bonus_Num
-from bands b
-join releases r on b.bid = r.bid
-join songs s on s.rid = r.rid
-where s.cdbonus='Y' and (r.rating >= 5 or r.rating is null)
-group by Band_Name
-order by Band_Name;
+SELECT b.name AS Band_Name, count(cdbonus) AS Bonus_Num
+FROM bands b
+JOIN releases r ON b.bid = r.bid
+JOIN songs s ON s.rid = r.rid
+WHERE s.cdbonus='Y' AND (r.rating >= 5 OR r.rating IS NULL)
+GROUP BY Band_Name
+ORDER BY Band_Name;
 
 
--- Query 7: we get all releases' years (which are not null) and select the bands' names that have all releases 
+-- Query 7: we get all releases' years (which are not null) and SELECT the bands' names that have all releases 
 --          launched after 1999
 
-SELECT b.name
+SELECT DISTINCT b.name
 FROM bands b
-WHERE 1999 < ALL (select year
-		  from releases r
-		  where b.bid=r.bid and year is not null);
+WHERE 1999 < ALL (SELECT year
+		  FROM releases r
+		  WHERE b.bid=r.bid AND year IS NOT NULL);
 
--- Query 8: we get all realeases' years (not null) and then select bands' names that have at least one release launched since 2011
+-- Query 8: we get all realeases' years (not null) and then SELECT bands' names that have at least one release launched since 2011
 
-SELECT b.name
+SELECT DISTINCT b.name
 FROM bands b
-WHERE 2011 <= ANY (select year
-		  from releases r
-		  where b.bid=r.bid and year is not null);				  
-
-
-   
+WHERE 2011 <= ANY (SELECT year
+		  FROM releases r
+		  WHERE b.bid=r.bid AND year IS NOT NULL);
