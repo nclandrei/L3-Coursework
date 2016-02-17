5b1278e5eeb99157c4717485d953f97860d9107a
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2016-02-17 17:24:18 +0000

    Added a C++ file reading a txt file and outputting the addresses correctly.

---
+++ NS/AX2/read_website_traces.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    ifstream in_file;
+    ofstream out_file;
+
+    char output_path[] = "traced-";
+    strcat(output_path, argv[1]);
+
+    string ip_addresses[30];
+    int count = 0;
+
+    in_file.open(argv[1]);
+    out_file.open(output_path);
+
+    string first_line;
+    getline(in_file, first_line);
+
+    for (string line; getline(in_file,line);) {
+        size_t found;
+        if ((found = line.find("*")) != string::npos) {
+            break;
+        }
+        int end_pos = line.find(" ", 5);
+        ip_addresses[count] = line.substr(4, end_pos-4);
+        ++count;
+    }
+
+    for (int i = 0; i < count - 1; ++i) {
+        out_file << "\"" << ip_addresses[i] << "\" -- \"" << ip_addresses[i+1] << "\"" << endl;
+    } 
+
+    in_file.close();
+    out_file.close();
+
+    return 0;
+}
