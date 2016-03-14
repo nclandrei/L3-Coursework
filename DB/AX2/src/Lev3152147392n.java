f79098a77d5e9f4928000a4221bb76e63b15c4a5
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2016-03-14 23:44:28 +0000

    Finished and submitted DB-AX2.

--- DB/AX2/src/Lev3152147392n.java
+++ DB/AX2/src/Lev3152147392n.java
@@ -1,20 +1,37 @@
 import java.sql.*;
 
+/**
+ * GUID: 2147392n
+ * DB(H) - Assessed Exercise 2
+ */
 public class Lev3152147392n {
 
-    public static void method3A(Statement statement) {
+    /**
+     * Provides the average rating of releases with a rating >=8
+     * @param statement the statement that will be used to execute the query
+     */
+    public static void Method3A(Statement statement) {
         String query = "SELECT AVG(RATING) AS AVERAGE FROM RELEASE WHERE RATING >= 8";
         try {
             ResultSet results = statement.executeQuery(query);
             while (results.next()) {
                 System.out.println(results.getString("AVERAGE") + "\n");
             }
-        } catch (SQLException e) {
+            System.out.print("\n");
+        }
+        catch (SQLException e) {
+            System.out.println("Failed to execute query for Method3A!\n");
             e.printStackTrace();
+            return;
         }
     }
 
-    public static void method3B(Statement statement) {
+    /**
+     * Provides the musicians' names that are still alive, are actively involved in more than one band
+     * and perform at more than one instrument
+     * @param statement the statement that will be used to execute the query
+     */   
+     public static void Method3B(Statement statement) {
         String query = "SELECT name " +
                 "FROM member " +
                 "JOIN memberof ON member.mid = memberof.mid " +
@@ -26,12 +43,20 @@ public class Lev3152147392n {
             while (results.next()) {
                 System.out.println(results.getString("name") + "\n");
             }
-        } catch (SQLException e) {
+            System.out.print("\n");
+        }
+        catch (SQLException e) {
+            System.out.println("Failed to execute query for Method3B!\n");
             e.printStackTrace();
+            return;
         }
     }
 
-    public static void method3C(Statement statement) {
+    /**
+     * Provides the band name(s) that have the maximum number of current members among all bands
+     * @param statement the statement that will be used to execute the query
+     */    
+     public static void Method3C(Statement statement) {
         String query = "SELECT name, count(DISTINCT memberof.mid) AS count_current_members " +
                 "FROM band " +
                 "JOIN memberof ON band.bid = memberof.bid " +
@@ -48,52 +73,85 @@ public class Lev3152147392n {
             while (results.next()) {
                 System.out.println(results.getString("name") + "\t" + results.getInt("count_current_members"));
             }
-        } catch (SQLException e) {
+            System.out.print("\n");
+        }
+        catch (SQLException e) {
+            System.out.println("Failed to execute query for Method3C\n");
             e.printStackTrace();
+            return;
         }
     }
 
     public static void main(String[] args) {
+
         try {
             Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
+        }
+        catch (ClassNotFoundException e) {
             System.out.println("Could not find JDBC Driver");
             e.printStackTrace();
             return;
         }
-        Connection connection = null;
+
+        Connection connection;
         try {
             connection =
                     DriverManager.getConnection("jdbc:postgresql://yacata.dcs.gla.ac.uk:5432/",
                             "lev3_15_2147392n", "2147392n");
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             System.out.println("Connection Failed!");
             e.printStackTrace();
             return;
         }
-        if (connection != null) {
-            System.out.println("Connection successful\n");
-        } else {
+
+        if (connection == null) {
             System.out.println("Failed to establish connection!");
         }
+
+        Statement statement;
         try {
-            assert connection != null;
-            Statement statement = connection.createStatement();
-
-            // running first SQL query
-            System.out.println("Results for Method3A\n");
-            method3A(statement);
-
-            // running second SQL query
-            System.out.println("Results for Method3B\n");
-            method3B(statement);
-
-            //running third SQL query
-            System.out.println("Results for Method3C\n");
-            method3C(statement);
-
-        } catch (SQLException e) {
+            statement = connection.createStatement();
+        }
+        catch (SQLException e) {
+            System.out.println("Failed to create statement!\n");
             e.printStackTrace();
+            return;
+        }
+
+        assert connection != null;
+
+        assert statement != null;
+
+        // running first SQL query
+        System.out.println("Results for Method3A: \n");
+            Method3A(statement);
+
+        // running second SQL query
+        System.out.println("Results for Method3B: \n");
+        Method3B(statement);
+
+        //running third SQL query
+        System.out.println("Results for Method3C: \n");
+        Method3C(statement);
+
+
+        try {
+            statement.close();
+        }
+        catch (SQLException e) {
+            System.out.println("Failed to close statement!\n");
+            e.printStackTrace();
+            return;
+        }
+
+        try {
+            connection.close();
+        }
+        catch (SQLException e) {
+            System.out.println("Failed to close the connection!\n");
+            e.printStackTrace();
+            return;
         }
     }
 }
