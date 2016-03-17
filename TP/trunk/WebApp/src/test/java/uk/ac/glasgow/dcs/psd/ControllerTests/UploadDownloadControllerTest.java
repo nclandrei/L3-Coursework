5b0003bfeacf853ccbeabb74ff8b9cb88bda233b
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2016-03-17 16:25:05 +0000

    Fixed bugs in the web app.

--- TP/trunk/WebApp/src/test/java/uk/ac/glasgow/dcs/psd/ControllerTests/UploadDownloadControllerTest.java
+++ TP/trunk/WebApp/src/test/java/uk/ac/glasgow/dcs/psd/ControllerTests/UploadDownloadControllerTest.java
@@ -1,29 +1,31 @@
 package uk.ac.glasgow.dcs.psd.ControllerTests;
 
         import org.junit.After;
-        import org.junit.Before;
-        import org.junit.Test;
-        import org.junit.runner.RunWith;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.beans.factory.support.ReplaceOverride;
-        import org.springframework.boot.test.SpringApplicationConfiguration;
-        import org.springframework.context.annotation.PropertySource;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.annotation.PropertySource;
         import org.springframework.http.MediaType;
+        import org.springframework.mock.web.MockMultipartFile;
         import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-        import org.springframework.test.context.web.WebAppConfiguration;
-        import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+        import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
         import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-        import org.springframework.web.context.WebApplicationContext;
-        import uk.ac.glasgow.dcs.psd.ApplicationConfiguration;
-        import uk.ac.glasgow.dcs.psd.Components.HelperComponent;
+import org.springframework.web.context.WebApplicationContext;
+import uk.ac.glasgow.dcs.psd.ApplicationConfiguration;
 
-        import java.io.File;
-        import java.io.IOException;
+        import java.io.*;
         import java.nio.file.Files;
-        import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardCopyOption;
 
-        import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-        import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+        import static org.hamcrest.Matchers.containsInAnyOrder;
+        import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Test case for HomeController
@@ -38,8 +40,17 @@ public class UploadDownloadControllerTest {
 
     private MockMvc mockMvc;
 
-    String publicResourcesTestZip = "";
+    String publicResourcesTestZip;
+    String publicResourcesSecondTestZip;
+    String getTestResourcesTestDocx;
+    String getTestResourcesTestPdf;
+    String getPublicResourcesTestDocx;
+    String getPublicResourcesTestPdf;
 
+
+    /**
+     * Setup files before each test
+     */
     @Before
     public void setup() {
         // for getFile()
@@ -48,7 +59,13 @@ public class UploadDownloadControllerTest {
         directoryTest += "/src/test/java/uk/ac/glasgow/dcs/psd";
         directoryMain += "/src/main/";
         String resourcesTestZip = directoryTest + "/Resources/testGetFile.zip";
-        publicResourcesTestZip += directoryMain + "resources/static/uploads/test.zip";
+        publicResourcesTestZip = directoryMain + "resources/static/uploads/test.zip";
+        getTestResourcesTestDocx = directoryTest + "/Resources/test.docx";
+        getTestResourcesTestPdf = directoryTest + "/Resources/sample70.pdf";
+        getPublicResourcesTestDocx = directoryMain + "resources/static/uploads/test.zip";
+        getPublicResourcesTestPdf = directoryMain + "resources/static/uploads/sample70.zip";
+        publicResourcesSecondTestZip =
+                directoryMain + "resources/static/uploads/Zfbj7AHekx.zip";
         try {
             Files.copy(new File(resourcesTestZip).toPath(),
                     new File(publicResourcesTestZip).toPath(),
@@ -59,10 +76,15 @@ public class UploadDownloadControllerTest {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
     }
 
+    /**
+     * Delete files after each test
+     */
     @After
-    public void tearDown(){
-        // remove test.zip
+    public void tearDown() {
         new File(publicResourcesTestZip).delete();
+        new File(publicResourcesSecondTestZip).delete();
+        new File(getPublicResourcesTestDocx).delete();
+        new File(getPublicResourcesTestPdf).delete();
     }
 
     /**
@@ -87,5 +109,124 @@ public class UploadDownloadControllerTest {
         this.mockMvc.perform(get("/tests")
                 .accept("application/zip"))
                 .andExpect(status().is4xxClientError());
+    }
+
+    /**
+     * Test working docx file
+     */
+    @Test
+    public void handleFileUploadDocx() {
+        try {
+            MockMultipartFile File = new MockMultipartFile("file",
+                    "test.docx",
+                    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                    new FileInputStream(getTestResourcesTestDocx));
+
+            MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+            mockMvc.perform(MockMvcRequestBuilders.fileUpload("/uploadFile")
+                    .file(File))
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                    .andExpect(jsonPath("$.status").value(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Test not working docx file
+     */
+    @Test
+    public void handleFileUploadDocxNotWorking() {
+        try {
+            MockMultipartFile File = new MockMultipartFile("file",
+                    null,
+                    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                    "non existing file".getBytes());
+
+            MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+            mockMvc.perform(MockMvcRequestBuilders.fileUpload("/uploadFile")
+                    .file(File))
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                    .andExpect(jsonPath("$.status").value(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Test working pdf file
+     */
+    @Test
+    public void handleFileUploadPdf() {
+        try {
+            MockMultipartFile File = new MockMultipartFile("file",
+                    "sample70.pdf",
+                    "application/pdf",
+                    new FileInputStream(getTestResourcesTestPdf));
+
+            MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+            mockMvc.perform(MockMvcRequestBuilders.fileUpload("/uploadFile")
+                    .file(File))
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                    .andExpect(jsonPath("$.status").value(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Test not working pdf file
+     */
+    @Test
+    public void handleFileUploadPdfNotWorking() {
+        try {
+            MockMultipartFile File = new MockMultipartFile("file",
+                    null,
+                    "application/pdf",
+                    "non existing file".getBytes());
+
+            MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+            mockMvc.perform(MockMvcRequestBuilders.fileUpload("/uploadFile")
+                    .file(File))
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                    .andExpect(jsonPath("$.status").value(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Test working link to a file
+     */
+    @Test
+    public void handleFileUploadDropbox() {
+        try {
+            this.mockMvc.perform(post("/uploadFileDropbox")
+                    .param("file","https://cldup.com/Zfbj7AHekx.docx")
+                    .param("fileName","Zfbj7AHekx.docx"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.status").value(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Test not working link to a file
+     */
+    @Test
+    public void handleFileUploadDropboxNotWorking() {
+        try {
+            this.mockMvc.perform(post("/uploadFileDropbox")
+                    .param("file","Zfbj7AHekx.docx")
+                    .param("fileName","Zfbj7AHekx.docx"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                    .andExpect(jsonPath("$.status").value(0));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
